acb08364714bbe969c89bb37d9aa4e8ec83e25cf
Carl Bernardo <carlbernrdo@gmail.com>
2020-05-29 13:46:45 -0700

    :pencil: update about me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,10 +21,10 @@ export default class About extends React.Component {
             <span role="img" aria-label="cheese">🧀</span> I went to school. <span className="about-body--years">2009</span>
           </h3>
           <p>
-            I attended the University of Wisconsin-Madison with the intent to be an International
-            Business and Japanese double major. I took a variety of courses from calculus/analytical geometry and
-            Asian-American history to economics and poetry during my Freshman year. I was accepted into the
-            Business School, but instead decided to pursue a B.S. in Economics thanks to Paul Krugman's enlightening textbook.
+            I attended <a href="https://www.wisc.edu/" target="_blank" rel="noopener noreferrer">some school in the Midwest</a> with the intent of majoring in International
+            Business and Japanese. I spent my freshman year taking a variety of courses from calculus/analytical geometry and
+            Asian-American history to economics and poetry. At the end of the year, I was accepted to the
+            Business School, but instead decided to pursue a degree in Economics thanks to <a href="https://en.wikipedia.org/wiki/Paul_Krugman" target="_blank" rel="noopener noreferrer">this NYTimes Columnist's</a> Textbook.
             If you didn't find me studying in the basement of Steenbock Library or at a table at <a
             href="https://goo.gl/maps/YgQPT2XCK8N2" target="_blank" rel="noopener noreferrer">my favorite coffee shop in Madison</a>,
             you could find me speaking Japanese at 会話の会 in Memorial Union or tutoring other students on how
@@ -34,12 +34,10 @@ export default class About extends React.Component {
             <span role="img" aria-label="high-speed-train">🚅</span> Did some cool internships. <span className="about-body--years">2009-2011</span>
           </h3>
           <p>
-            In 2010, I interned at Central Japan Railways (JR Central), Japan's highest throughput high-speed-rail
-            operator (that's a fancy way of saying their most popular train company). I lived in a little apartment
-            near Shinagawa Station in Tokyo for 3 months during that summer and conducted field work analyzing JR Central's
-            marketing strategies, ridership, and throughput. I eventually presented my research to their International Division in Japanese.
-            The following summer I participated in Capital One's Summit for Developing Leaders, a one-week intensive case study competition,
-            where my team placed 2nd and took home some sweet <i>vintage</i> iPod Shuffles as our prize.
+            In 2010, I interned at <a href="https://global.jr-central.co.jp/en/" target="_blank" rel="noopener noreferrer">a cool train company in Tokyo</a>. I lived in a little apartment
+            near Shinagawa Station in Tokyo for 3 months during the summer and conducted field work analyzing their
+            marketing strategies, ridership, and throughput. The following summer I participated in <a href="https://campus.capitalone.com/summits/" target="_blank" rel="noopener noreferrer">Capital
+            One's Summit for Developing Leaders</a> where my team placed 2nd and took home some sweet <i>vintage</i> iPod Shuffles as our prize.
           </p>
           <h3>
             <span role="img" aria-label="flag-jpn">🇯🇵</span> Lived in 日本. <span className="about-body--years">2011-2012</span>
@@ -48,8 +46,8 @@ export default class About extends React.Component {
             In 2011, I left to study abroad at <a href="https://www.keio.ac.jp/en/" target="_blank" rel="noopener noreferrer">Keio
             University</a> in Tokyo (go Unicorns - <i>seriously</i>, their mascot is a Unicorn) as a Monbukagakusho
             Scholar. During that year, I built my proficiency in Level 9 of Keio's Japanese Language Program, passed N2
-            of the JLPT, and played on one of Keio's intramural basketball teams. I also spent a lot of time traveling
-            around Japan, especially obscure areas like Yakushima, Mt. Aso, and Takayama.
+            of the JLPT, and played on one of Keio's intramural basketball teams. I also spent lots of early mornings at Gusto making use of
+            their unlimited drink bar (non-alcoholic of course).
           </p>
           <h3>
             <span role="img" aria-label="male-graduate">👨‍🎓</span> Graduated. And flew to the East Coast. <span className="about-body--years">2013 - 2015</span>
@@ -62,8 +60,8 @@ export default class About extends React.Component {
           </p>
           <p>
             After graduating, I moved to the East Coast and worked as an Associate Consultant
-            at <a href="https://www.thefrontierproject.com/" target="_blank" rel="noopener noreferrer">The Frontier Project</a>, a
-            boutique consulting firm located in Richmond, VA. During my time there, I designed training programs that
+            at <a href="https://www.thefrontierproject.com/" target="_blank" rel="noopener noreferrer">a
+            boutique consulting firm</a> located in Richmond, VA. During my time there, I designed training programs that
             made use of Behavioral Economics to incentivize human behavior and made data-driven recommendations for
             client projects. I began building tools to support my data analysis and taking on web-based projects. My
             role shifted from business facing activities to creating web applications for our clients using JavaScript/jQuery,
@@ -77,7 +75,7 @@ export default class About extends React.Component {
             In the Fall of 2015, I moved across the country from Richmond, VA to Los Angeles, CA.
           </p>
           <p>
-            While working remote for The Frontier Project on some smaller web projects, I continued to grow my coding
+            While working remote for my company on some smaller web projects, I continued to grow my coding
             skillset and worked with <a href="https://www.linkedin.com/in/shin-adachi-1b6bb1113/" target="_blank" rel="noopener noreferrer">this
             awesome person</a>, <a href="https://www.linkedin.com/in/daisytsao/" target="_blank" rel="noopener noreferrer">this awesome person</a>,
             and <a href="https://www.linkedin.com/in/justanman/" target="_blank" rel="noopener noreferrer">this awesome person</a> on a handful of
@@ -85,13 +83,11 @@ export default class About extends React.Component {
             of which was even recognized by Yelp</a>. After 2 years of working at The Frontier Project, I left for the
             opportunity to be Software Engineer #10 and work on building out <a href="https://my.socialnative.com/" target="_blank" rel="noopener noreferrer">the web
             application</a> of a data-first ad startup,
-            which <a href="https://techcrunch.com/2017/05/18/social-native-raises-8-million-to-build-a-new-social-media-ad-platform/" target="_blank" rel="noopener noreferrer">raised an $8M seed round</a> during
+            where we received <a href="https://techcrunch.com/2017/05/18/social-native-raises-8-million-to-build-a-new-social-media-ad-platform/" target="_blank" rel="noopener noreferrer">an $8M seed round</a> during
             my time there.
           </p>
           <p>
-            <a href="http://socialnative.com/" target="_blank" rel="noopener noreferrer">Social Native</a> helped brands connect with
-            consumers by creating 1:1 personalized content on demand. As a Software Engineer there, I worked across the stack,
-            building services for our internal API and integrating with third-party platforms such as Typeform and
+            As a Software Engineer there, I worked across the stack, building services for our internal API and integrating with third-party platforms such as Typeform and
             Filestack for data collection and file uploads to building out internal, brand, and creator-facing features
             on our platform. We used a mix of MySQL, Elasticsearch, and Redis for our data storage, Node.js/Koa.js for
             our API, PHP/Laravel for routing/controller actions, and React/Redux for our view layer. We did unit,
@@ -99,15 +95,14 @@ export default class About extends React.Component {
             coverage across our projects.
           </p>
           <p>
-            After Social Native, I decided to follow my curiosity and started pursuing a Master of Science in Computer
+            After that, I decided to follow my curiosity and started pursuing a Master of Science in Computer
             Science at <a href="http://www.omscs.gatech.edu/" target="_blank" rel="noopener noreferrer">Georgia Tech (OMSCS)</a>. I also took some time to travel to Seattle, WA and Austin, TX. Oh, and I ran
             my first half-marathon (2:37:49) in San Diego.
           </p>
           <p>
             While continuing my studies at Georgia Tech, I worked remotely for a company based in San Francisco that enables
-            better career outcomes for hourly workers by providing a transparent and efficient labor marketplace.
-            At <a href="https://www.workstep.com/" target="_blank" rel="noopener noreferrer">WorkStep</a>, we used
-            MySQL/Memcached for our data storage, Python/WebApp2 for our APIs, and React/Redux/Redux Saga for our view layer.
+            better career outcomes for hourly workers by providing a transparent and efficient labor marketplace. <a href="https://www.workstep.com/" target="_blank" rel="noopener noreferrer">There</a>, 
+            we used MySQL/Memcached for our data storage, Python/WebApp2 for our APIs, and React/Redux/Redux Saga for our view layer.
             I worked across all parts of the application and helped re-architect our employer interview scheduling feature,
             and was a major contributor to the new employer self-service flow, which doubled our customer success teams
             productivity (measured in time spent onboarding new employer customers).
@@ -116,15 +111,23 @@ export default class About extends React.Component {
             <span role="img" aria-label="male-facepalm">🤦🏻</span> Here's the cool stuff I'm doing now. <span className="about-body--years">2020-current</span>
           </h3>
           <p>
-            I'm currently consulting for a mobile technology and data science company oriented around financial services in emerging markets. <b>I
-            am <i>not actively looking</i> for new opportunities, but am open to more consulting work or remote full-time positions with
-            companies that have secured a Series A</b> (still reach out if you think my skillset matches what you're looking for - I don't bite).
+            I'm currently consulting for a mobile tech and data science company oriented around financial services in emerging markets.
           </p>
-          <p>Here's what else is happening...</p>
+          <p>
+            I am <b>NOT</b> actively looking for new opportunities, but am open to more consulting work or remote full-time positions with
+            companies that have secured a Series A (still reach out if you think my skillset matches what you're looking for - I don't bite). I'm
+            a HUGE fan of JavaScript (particularly the React Ecosystem and Vue.js) and get overly excited about <a href="https://deno.land/"  target="_blank" rel="noopener noreferrer">Deno</a>. I would
+            LOVE to work with Python again or even dabble with some Golang. And I respect the tried and true Java.
+          </p>
+          <p>
+            Here are things I'm currently reading (<span role="img" aria-label="books">📚</span>), 
+            watching (<span role="img" aria-label="eyes-headphones">👀</span>), and 
+            playing (<span role="img" aria-label="games">🎮</span>), and planning to go to (<span role="img" aria-label="plane-departure">🛫</span>):
+          </p>
           <ul>
             <li>
               <span role="img" aria-label="books">📚 </span>
-              Killing Commendatore, Digital Minimalism, and The Institute
+              Digital Minimalism, The Institute, and The Name of the Wind
             </li>
             <li>
               <span role="img" aria-label="eyes-headphones">👀 </span>
@@ -132,7 +135,7 @@ export default class About extends React.Component {
             </li>
             <li>
               <span role="img" aria-label="games">🎮 </span>
-              Spirit Island and Animal Crossing: New Horizons
+              Valorant, Spirit Island, Animal Crossing: New Horizons
             </li>
             <li>
               <span role="img" aria-label="plane-departure">🛫 </span>
@@ -168,7 +171,7 @@ export default class About extends React.Component {
             <i>- Sheryl Sandberg</i>
           </p>
           <p>
-            Wrong. You don't blindly get on a rocket ship when you don't know who built it, who's flying it, who else is on it, where it's going, and how much fuel is in the tank.
+            Shut up. You don't blindly get on a rocket ship when you don't know who built it, who's flying it, who else is on it, where it's going, and how much fuel is in the tank.
           </p>
           <p>
             <b>3. Book smart or street smart?</b>
@@ -194,13 +197,14 @@ export default class About extends React.Component {
             over again.
           </p>
           <p>
-            I wholeheartedly believe there are products for which there is no market for regardless of how great the team is who built it.
+            I wholeheartedly believe there are products for which there is no market for regardless of how great the team is who built it. I don't work with
+            founders who are too blind to see that. If this offends you, we probably shouldn't work together.
           </p>
           <p>
             <b>6. Where is your happy place?</b>
           </p>
           <p>
-            Palm Springs, CA. Ace Hotel. Poolside with a good book and a pina colada.
+            Palm Springs, CA. Ace Hotel. Poolside. Good book in one hand; pina colada in the other.
           </p>
           <p>
             <b>7. What is your go to comfort food?</b>
